498582f748162d39b87504ba993cb5dea9b7a691
seahahn <seah.ahn.nt@gmail.com>
2022-02-16 16:04:38 +0900

    Revert "Navbar & Left Sidebar(Dropdown) added"
    This reverts commit 79b6ba8bfc7a1b41eb537e228f824674f02e7c83.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
-import Navbar from "./Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
