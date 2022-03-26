ad457f198c69429fe5678ced6c92c08125ce85be
seahahn <seah.ahn.nt@gmail.com>
2022-03-26 16:27:31 +0900

    networkConfig updated

--- src/utils/networkConfigs.js
+++ src/utils/networkConfigs.js
@@ -1,4 +1,4 @@
-export const CORS = "cors"; // CORS 설정
+export const CORS = process.env.REACT_APP_CORS; // CORS 설정
 
 export const UPLOAD_ACCEPT = "application/vnd.ms-excel, text/csv"; // 데이터셋 업로드 시 파일 확장자 설정
 
