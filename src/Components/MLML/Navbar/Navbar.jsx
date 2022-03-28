4eba851639862838e05906a645d0d23e1e056f59
seahahn <seah.ahn.nt@gmail.com>
2022-03-29 02:44:08 +0900

    init refresh token func logging

--- src/Components/MLML/Navbar/Navbar.jsx
+++ src/Components/MLML/Navbar/Navbar.jsx
@@ -36,6 +36,7 @@ function Navbar() {
     if (userIdx) {
       // 로컬 스토리지에 저장된 사용자 고유 번호가 있는 경우 토큰 갱신 요청
       refreshToken().then((res) => {
+        console.log(res);
         if (res) {
           // 토큰 갱신 요청 성공한 경우
           refreshTokenInterval.current = setInterval(refreshToken, 1000 * 60 * 1);
