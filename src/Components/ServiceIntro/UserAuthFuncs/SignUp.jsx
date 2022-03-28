a989df328c4f65b5cf7ca988b615918f5ecdd0a6
seahahn <seah.ahn.nt@gmail.com>
2022-03-28 16:19:30 +0900

    Bug fixed

--- src/Components/ServiceIntro/UserAuthFuncs/SignUp.jsx
+++ src/Components/ServiceIntro/UserAuthFuncs/SignUp.jsx
@@ -66,7 +66,7 @@ function SignUp({ isOpen, setIsOpen, setIsSignInOpen }) {
         setNicknameChecked(false);
         break;
       default:
-        console.log("error");
+        // console.log("error");
         break;
     }
     setInput({
