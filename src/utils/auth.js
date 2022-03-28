b4977cf77f962966ecb746df79cc0440049179d2
seahahn <seah.ahn.nt@gmail.com>
2022-03-29 03:53:45 +0900

    init refresh token func logging

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -23,10 +23,10 @@ export const refreshToken = async () => {
       if (res.result) {
         console.log("refreshToken success", res.result);
         localStorage.setItem("AIPLAY_USER_TOKEN", res.token);
-        return res.token_state;
+        return res.result;
       } else {
         console.log("refreshToken error: ", res.message);
-        return res.token_state;
+        return res.result;
       }
     })
     .catch((err) => {
