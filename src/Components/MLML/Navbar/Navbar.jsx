79c0e8ce532a5491cd4eb16d14b468bbd4c76115
seahahn <seah.ahn.nt@gmail.com>
2022-03-29 06:22:03 +0900

    init refresh token func updated

--- src/Components/MLML/Navbar/Navbar.jsx
+++ src/Components/MLML/Navbar/Navbar.jsx
@@ -35,9 +35,9 @@ function Navbar() {
     // 새로고침 등으로 페이지가 새로 로딩된 경우 JWT 발급 재시작
     if (userIdx) {
       // 로컬 스토리지에 저장된 사용자 고유 번호가 있는 경우 토큰 갱신 요청
-      const tokenStateRes = refreshToken();
+      const tokenStateRes = await refreshToken();
       console.log(tokenStateRes);
-      const tokenState = tokenStateRes.json();
+      const tokenState = await tokenStateRes.json();
       console.log(tokenState);
       if (tokenState.result) {
         // 토큰 갱신 요청 성공한 경우
